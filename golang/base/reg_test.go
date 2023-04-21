4b4c2efe8eb6daef42380e7fd9b28d3917d3ca9a
oldme <tyyn1022@163.com>
2023-04-22 01:46:00 +0800

    - 对goframe文档正则的修正 - 测试go正则的基础用法

---
+++ golang/base/reg_test.go
@@ -0,0 +1,39 @@
+// regexp 包测试
+package test
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+// 字符串是否能匹配到正则表达式
+func TestMatch(t *testing.T) {
+	mStr, _ := regexp.MatchString("me", "oldme")
+	fmt.Println(mStr)
+	mByte, _ := regexp.Match("me", []byte("oldme"))
+	fmt.Println(mByte)
+}
+
+// 可以用 Compile 来准备一个正则对象
+func TestCompile(t *testing.T) {
+	c, _ := regexp.Compile("me")
+	fmt.Println(c.MatchString("oldme"))
+}
+
+// 匹配到字符串
+func TestFindString(t *testing.T) {
+	c, _ := regexp.Compile("me")
+	s := c.FindString("oldme")
+	// 字符串第一次出现的位置
+	i := c.FindStringIndex("oldme")
+	fmt.Println(s)
+	fmt.Println(i)
+}
+
+// 匹配到所有的子字符串，即括号中的正则表达式
+func TestStringSubmatch(t *testing.T) {
+	c, _ := regexp.Compile("([a-z]+)\\d+")
+	s := c.FindStringSubmatch("oldme123")
+	fmt.Println(s)
+}
---
+++ golang/goframe/reg_test.go
@@ -0,0 +1,44 @@
+package goframe
+
+import (
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/text/gregex"
+	"testing"
+)
+
+func TestReplaceFunc(t *testing.T) {
+	var (
+		patternStr = `([A-Z])\w+`
+		str        = "hello Golang 2018~2021!"
+	)
+	// In contrast to [ExampleReplaceFunc]
+	// the result contains the `pattern' of all subpatterns that use the matching function
+	result, err := gregex.ReplaceFuncMatch(patternStr, []byte(str), func(match [][]byte) []byte {
+		g.Dump(match)
+		return []byte("Gf")
+	})
+	g.Dump(result)
+	g.Dump(err)
+
+	// ReplaceStringFuncMatch
+	resultStr, err := gregex.ReplaceStringFuncMatch(patternStr, str, func(match []string) string {
+		g.Dump(match)
+		match[0] = "Gf"
+		return match[0]
+	})
+	g.Dump(resultStr)
+	g.Dump(err)
+}
+
+// [
+// 	"Golang",
+// 	"G",
+// ]
+// "hello Gf 2018~2021!"
+// <nil>
+// [
+// 	"Golang",
+// 	"G",
+// ]
+// "hello Gf 2018~2021!"
+// <nil>
