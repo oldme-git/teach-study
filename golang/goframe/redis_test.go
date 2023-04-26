fd7af3d6899635ddd7807eb0bb045c3bef409701
oldme <tyyn1022@gmail.com>
2023-04-26 11:26:21 +0800

    - 提交若干测试文件

---
+++ golang/goframe/redis_test.go
@@ -0,0 +1,21 @@
+package goframe
+
+import (
+	_ "github.com/gogf/gf/contrib/nosql/redis/v2"
+	"github.com/gogf/gf/v2/frame/g"
+	"testing"
+)
+
+type RedisTest struct {
+	id    int
+	title string
+}
+
+func TestRedisSet(t *testing.T) {
+	redis := g.Redis()
+	rt := &RedisTest{
+		id:    1,
+		title: "title",
+	}
+	redis.Set(ctx, "key", rt)
+}
