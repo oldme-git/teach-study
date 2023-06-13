abfe2150a6f8e810af495be6ef135deb6959cb7e
oldme <tyyn1022@gmail.com>
2023-06-13 14:38:04 +0800

    整理php

---
+++ php/base/di.php
@@ -0,0 +1,14 @@
+<?php
+use Psr\Container\ContainerInterface;
+
+class MyDi implements ContainerInterface {
+    public function get($id)
+    {
+//        return 0;
+    }
+
+    public function has($id)
+    {
+        // TODO: Implement has() method.
+    }
+}
---
+++ php/base/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '00b6a26313702715bf319856d7c67500ed6075d0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '00b6a26313702715bf319856d7c67500ed6075d0',
+            'dev_requirement' => false,
+        ),
+        'psr/container' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/container',
+            'aliases' => array(),
+            'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
