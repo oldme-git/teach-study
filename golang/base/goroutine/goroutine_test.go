85d51ab3c70d9ef899d488efa455dbcc5a7c7869
oldme <tyyn1022@gmail.com>
2023-07-18 10:13:36 +0800

    TestTimeSinceWithGMP

--- golang/base/goroutine/goroutine_test.go
+++ golang/base/goroutine/goroutine_test.go
@@ -3,6 +3,8 @@ package goroutine
 import (
 	"fmt"
 	"runtime"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -27,4 +29,36 @@ func TestGoNum(t *testing.T) {
 	ch <- true
 	time.Sleep(1 * time.Second)
 	fmt.Println(runtime.NumGoroutine())
+}
+
+// 测试time.Since是否会受到抢占式调度的影响
+func TestTimeSinceWithGMP(t *testing.T) {
+	runtime.GOMAXPROCS(1)
+	var (
+		sMap sync.Map
+		wg   = &sync.WaitGroup{}
+		num  = 5000000
+	)
+
+	wg.Add(num)
+	for i := 0; i < num; i++ {
+		go func(i int) {
+			defer wg.Done()
+			start := time.Now()
+			// 模拟一个耗时请求
+			time.Sleep(3 * time.Second)
+			elapsed := time.Since(start).Seconds()
+			sMap.Store(strconv.Itoa(i), elapsed)
+		}(i)
+	}
+	wg.Wait()
+
+	j := 0
+	sMap.Range(func(key, value any) bool {
+		if value.(float64) > 4 {
+			j++
+		}
+		return true
+	})
+	fmt.Printf("有 %d 个异常", j)
 }
