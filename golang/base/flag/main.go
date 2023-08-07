90ca3eba1fb7baf58fcb1dee82520117cfa49534
oldme <tyyn1022@gmail.com>
2023-08-07 09:54:17 +0800

    flag使用

--- golang/base/flag/main.go
+++ golang/base/flag/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	var (
+		host string
+		name int
+	)
+	flag.StringVar(&host, "host", "", "地址")
+	flag.IntVar(&name, "name", 12, "名称")
+	port := flag.String("port", "80", "端口")
+	flag.Parse()
+	fmt.Printf("host: %s\n", host)
+	fmt.Printf("name: %d\n", name)
+	fmt.Printf("port: %s\n", *port)
 }
