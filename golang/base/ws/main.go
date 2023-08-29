c7cb6bae2b6c5ce7d82d944dcd024a5c5fe970d4
oldme <tyyn1022@gmail.com>
2023-08-29 09:48:00 +0800

    ws

--- golang/base/ws/main.go
+++ golang/base/ws/main.go
@@ -12,13 +12,14 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-var wssrv *websocket.Conn
-var conn map[int]int
+var clients = make(map[*websocket.Conn]struct{})
 
 func main() {
 	http.HandleFunc("/", home)
 	// 定义ws路径
 	http.HandleFunc("/ws", ws)
+	// 广播
+	http.HandleFunc("/broadcast", broadcast)
 	err := http.ListenAndServe("localhost:18000", nil)
 	if err != nil {
 		panic(err)
@@ -29,27 +30,37 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello"))
 }
 
+func broadcast(w http.ResponseWriter, r *http.Request) {
+	for client, _ := range clients {
+		client.WriteMessage(websocket.TextMessage, []byte("能听到我的广播吗"))
+	}
+}
+
 func ws(w http.ResponseWriter, r *http.Request) {
 	// 将http服务升级成ws服务
-	wssrv, err := upgrader.Upgrade(w, r, nil)
+	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-	defer wssrv.Close()
+	// 建立连接将客户端添加到map中
+	clients[c] = struct{}{}
+	defer c.Close()
 	for {
 		// 监听消息
-		mt, message, err := wssrv.ReadMessage()
+		mt, message, err := c.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
 		log.Printf("recv: %s", message)
 		// 监听到信息，向客户端响应
-		err = wssrv.WriteMessage(mt, message)
+		err = c.WriteMessage(mt, message)
 		if err != nil {
 			log.Println("write:", err)
 			break
 		}
 	}
+	// 关闭连接将客户端从map中剔除
+	delete(clients, c)
 }
