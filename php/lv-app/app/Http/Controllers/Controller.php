ff1a95df43ea93b95198676fcb028c49f759205d
oldme <tyyn1022@gmail.com>
2024-01-31 14:09:07 +0800

    update lv version

--- php/lv-app/app/Http/Controllers/Controller.php
+++ php/lv-app/app/Http/Controllers/Controller.php
@@ -3,16 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function mongoTest()
-    {
-        return "hello";
-    }
+    use AuthorizesRequests, ValidatesRequests;
 }
