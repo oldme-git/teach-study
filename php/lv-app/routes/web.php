39fc245b9079c5d71010bdae548d87eb122ae9e7
oldme <tyyn1022@gmail.com>
2024-01-31 14:45:59 +0800

    install mongo-laravel

--- php/lv-app/routes/web.php
+++ php/lv-app/routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/mongo", [\App\Http\Controllers\Controller::class, "mongoTest"]);
