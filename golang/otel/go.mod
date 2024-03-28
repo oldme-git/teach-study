971a6bba5f4aa0183a43548431de2ce0b323056c
oldme <tyyn1022@gmail.com>
2024-03-28 14:44:33 +0800

    假如grpc上报代码

--- golang/otel/go.mod
+++ golang/otel/go.mod
@@ -4,14 +4,9 @@ go 1.22.1
 
 require (
 	go.opentelemetry.io/otel v1.24.0
-	go.opentelemetry.io/otel/exporters/jaeger v1.17.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.24.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.24.0
-	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.24.0
-	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.24.0
-	go.opentelemetry.io/otel/metric v1.24.0
 	go.opentelemetry.io/otel/sdk v1.24.0
-	go.opentelemetry.io/otel/sdk/metric v1.24.0
-	go.opentelemetry.io/otel/trace v1.24.0
 )
 
 require (
@@ -21,6 +16,8 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/otel/trace v1.24.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.1.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
--- golang/otel/trace/trace_test.go
+++ golang/otel/trace/trace_test.go
@@ -7,25 +7,19 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 )
 
-func TestTrace(t *testing.T) {
+func TestTraceHttp(t *testing.T) {
 	ctx := context.Background()
 
 	// 创建 OTLP HTTP 导出器，连接到 Jaeger
-	// exporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpointURL("http://192.168.10.41:4318/v1/traces"))
-	traceClientHTTP := otlptracehttp.NewClient(
-		otlptracehttp.WithEndpoint("192.168.10.42:4318"),
-		otlptracehttp.WithURLPath("/v1/traces"),
-		otlptracehttp.WithInsecure(),
-		otlptracehttp.WithCompression(1),
-	)
-	exporter, err := otlptrace.New(ctx, traceClientHTTP)
+	exporter, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpointURL("http://192.168.10.41:4318/v1/traces"))
+
 	if err != nil {
 		log.Fatalf("创建导出器失败: %v", err)
 	}
@@ -33,7 +27,59 @@ func TestTrace(t *testing.T) {
 	// 创建资源
 	res, err := resource.New(ctx,
 		resource.WithAttributes(
-			semconv.ServiceNameKey.String("otel-traces-demo"),
+			semconv.ServiceNameKey.String("otel-traces-demo-http"),
+		),
+	)
+	if err != nil {
+		log.Fatalf("创建资源失败: %v", err)
+	}
+
+	// 创建 Tracer 提供器
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+
+	// 设置全局 Tracer 提供器
+	otel.SetTracerProvider(tp)
+
+	// 创建一个新的 trace
+	tracer := otel.Tracer("example-tracer")
+	ctx, span := tracer.Start(ctx, "root-span")
+	// 暂停 100ms
+	time.Sleep(100 * time.Millisecond)
+	// 结束 span
+	span.End()
+
+	// 创建子span
+	_, childSpan := tracer.Start(ctx, "child-span")
+	// 暂停 50ms
+	time.Sleep(50 * time.Millisecond)
+	childSpan.End()
+
+	// 确保所有的 spans 都被发送
+	if err := tp.Shutdown(ctx); err != nil {
+		log.Fatalf("关闭 Tracer 提供器失败: %v", err)
+	}
+}
+
+func TestTraceGrpc(t *testing.T) {
+	ctx := context.Background()
+
+	// 创建 OTLP gRPC 导出器，连接到 Jaeger
+	exporter, err := otlptracegrpc.New(ctx,
+		otlptracegrpc.WithEndpoint("192.168.10.41:4317"),
+		otlptracegrpc.WithInsecure(),
+	)
+
+	if err != nil {
+		log.Fatalf("创建导出器失败: %v", err)
+	}
+
+	// 创建资源
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String("otel-traces-demo-grpc"),
 		),
 	)
 	if err != nil {
