0d1b0c4d047f6f5cb24f292c5352b278d9e43eff
oldme <tyyn1022@gmail.com>
2024-08-14 15:09:08 +0800

    prometheus 加入 Histogram

--- golang/promethues/main.go
+++ golang/promethues/main.go
@@ -14,7 +14,6 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-
 	"go.opentelemetry.io/otel/exporters/prometheus"
 	api "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/sdk/metric"
@@ -22,7 +21,10 @@ import (
 
 const meterName = "oldme_prometheus_testing"
 
-var requestHelloCounter api.Int64Counter
+var (
+	requestHelloCounter      api.Int64Counter
+	requestDurationHistogram api.Float64Histogram
+)
 
 func main() {
 	ctx := context.Background()
@@ -43,6 +45,16 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// 创建 Histogram 指标类型
+	requestDurationHistogram, err = meter.Float64Histogram(
+		"request_hello_duration_seconds",
+		api.WithDescription("记录 Hello 请求的耗时统计"),
+		api.WithExplicitBucketBoundaries(0.1, 0.2, 0.5, 1, 2, 5),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	go serveMetrics()
 	go goroutineMock()
 
@@ -54,9 +66,19 @@ func serveMetrics() {
 	log.Printf("serving metrics at localhost:2223/metrics")
 	http.Handle("/metrics", promhttp.Handler())
 
-	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/index", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 记录 counter 指标
 		requestHelloCounter.Add(r.Context(), 1)
+
+		// 计算请求处理时间
+		startTime := time.Now()
+		// 模拟请求处理时间
+		time.Sleep(time.Duration(rand.Intn(3)) * time.Second)
+		defer func() {
+			duration := time.Since(startTime).Seconds()
+			requestDurationHistogram.Record(r.Context(), duration)
+		}()
+
 		_, _ = w.Write([]byte("Hello, Otel!"))
 	}))
 
